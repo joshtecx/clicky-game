124f5f2fdd8af8d415046fb76ba8942268b1f50a
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-06-25 11:22:59 -0400

    Added some more stying

--- clicky/src/components/ImageCard/ImageCard.js
+++ clicky/src/components/ImageCard/ImageCard.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import "./ImageCard.css"
 const ImageCard = props => (
-    <div className="card col-2">
-        <div className="img-container">
-            <img  src={props.image}   onClick={() => props.clicker(props.id, props.clicked)} data-clicked={props.clicked}/>
+    <div className="row">
+        <div className="card col-2">
+            <div className="img-container">
+                <img  src={props.image}   onClick={() => props.clicker(props.id, props.clicked)} data-clicked={props.clicked}/>
+            </div>  
         </div>
-   
     </div>
 );
     
